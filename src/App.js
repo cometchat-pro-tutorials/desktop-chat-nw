fd2ae124263caab95ed8267c6059823bf4132b0c
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-03 17:55:44 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import ChatPane from "./components/ChatPane";
+import Welcome from "./components/Welcome";
+import Header from "./components//Header";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
+  const name = "Name";
+
   return (
-    <div className="App">
-      Desktop chat
+    <div className="window">
+      <Header />
+      <div className="window-content">
+        {name ? <ChatPane /> : <Welcome />}
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
