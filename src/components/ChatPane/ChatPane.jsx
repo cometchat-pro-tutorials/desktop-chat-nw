8de317a411e302612916cde6c47c5e0dd81f577b
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-06 15:00:27 +0200

    Update ChatPane.jsx

--- src/components/ChatPane/ChatPane.jsx
+++ src/components/ChatPane/ChatPane.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
 import Participants from "../Participants/Participants";
 import Conversation from "../Conversation/Conversation";
+import { init, login } from '../../actions';
 import "./ChatPane.css";
 
-const ChatPane = () => (
-  <div className="chat-pane">
-    <Participants/>
-    <Conversation/>
-  </div>
-);
-export default ChatPane;
+const ChatPane = ({init, login}) => {
+
+    useEffect(() => {
+        init().then(login);
+    });
+
+    return (
+        <div className="chat-pane">
+            <Participants />
+            <Conversation />
+        </div>);
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    init: () => dispatch(init()),
+    login: () => dispatch(login())
+});
+
+export default connect(null, mapDispatchToProps)(ChatPane);
