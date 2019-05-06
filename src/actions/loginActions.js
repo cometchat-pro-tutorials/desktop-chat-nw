1039a8963f05f0f8fb9f461efce59527cd1d89f2
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-06 14:59:37 +0200

    Update loginActions.js

--- src/actions/loginActions.js
+++ src/actions/loginActions.js
@@ -1,3 +1,5 @@
+import { loginChat } from '../chat-api';
+
 export const LOGIN_BEGIN = 'LOGIN_BEGIN';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
@@ -13,3 +15,18 @@ export const loginSuccess = () => ({
 export const loginFailed = () => ({
   type: LOGIN_FAILED
 });
+
+export const login = () => {
+    return dispatch => {
+        dispatch(loginBegin());
+        return loginChat()
+            .then(json => {
+                dispatch(loginSuccess());
+                return json;
+            })
+            .catch(error =>
+                dispatch(loginFailed(error))
+            );
+    };
+};
+
