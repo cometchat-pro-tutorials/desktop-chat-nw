72f3a8255fc987dbd7b54681943d7b9ad14a6a4d
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-06 11:15:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 import ChatPane from "./components/ChatPane/ChatPane";
 import Welcome from "./components/Welcome/Welcome";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { readRecord } from "./utils/localStorageService";
 
 const App = () => {
-  const name = "Name";
+    const [hasName, setHasName] = useState(readRecord('username'));
 
-  return (
-    <>
-      <Header />
-        {name ? <ChatPane /> : <Welcome />}
-      <Footer />
-    </>
-  );
+    const readName = () => {
+        setHasName(readRecord('username') !== null);
+    };
+
+    return (
+        <>
+            <Header />
+            {hasName ? <ChatPane /> : <Welcome callback={readName} />}
+            <Footer />
+        </>
+    );
 };
 
 export default App;
