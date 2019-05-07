dd19e337d7194db0fdc33546292c02d0b7238738
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-07 15:22:30 +0200

    Updated actions.

--- src/actions/messageActions.js
+++ src/actions/messageActions.js
@@ -1,35 +1,36 @@
-import { sendChatMessage } from "../chat-api";
+// import { sendChatMessage } from "../chat-api";
+import { sendChatMessage } from "../chat-api/mocks";
 
 export const SEND_MESSAGE_BEGIN = 'SEND_MESSAGE_BEGIN';
 export const SEND_MESSAGE_SUCCESS = 'SEND_MESSAGE_SUCCESS';
 export const SEND_MESSAGE_FAILED = 'SEND_MESSAGE_FAILED';
 
 
-export const sendMessageBegin = message => ({
-  type: SEND_MESSAGE_BEGIN,
-  payload: {message}
+export const sendMessageBegin = () => ({
+    type: SEND_MESSAGE_BEGIN
 });
 
-export const sendMessageSuccess = () => ({
-  type: SEND_MESSAGE_SUCCESS
+export const sendMessageSuccess = ({message, username}) => ({
+    type: SEND_MESSAGE_SUCCESS,
+    payload: { message, username }
 });
 
 export const sendMessageFailed = error => ({
-  type: SEND_MESSAGE_FAILED,
-  payload: {error}
+    type: SEND_MESSAGE_FAILED,
+    payload: { error }
 });
 
 
 export const sendMessage = message => {
-  return dispatch => {
-    dispatch(sendMessageBegin(message));
-    return sendChatMessage(message)
-      .then(json => {
-        dispatch(sendMessageSuccess());
-        return json;
-      })
-      .catch(error =>
-        dispatch(sendMessageFailed(error))
-      );
-  };
+    return dispatch => {
+        dispatch(sendMessageBegin());
+        return sendChatMessage(message)
+            .then(json => {
+                dispatch(sendMessageSuccess(message));
+                return json;
+            })
+            .catch(error =>
+                dispatch(sendMessageFailed(error))
+            );
+    };
 };
