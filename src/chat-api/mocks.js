b6a42a8a2ddc885b0a2f8b733f7cf4313f3b3444
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-08 10:48:12 +0200

    Added users list mock.

--- src/chat-api/mocks.js
+++ src/chat-api/mocks.js
@@ -1,20 +1,126 @@
 import { CometChat } from "@cometchat-pro/chat";
 
-const mockedData = [
-    {name: 'Sub-Zero', role: 'Cold Ninja'},
-    {name: 'Scorpion', role: 'Hot Ninja'},
-    {name: 'Johnny Cage', role: 'Law Enforcer'},
-    {name: 'Sonya', role: 'Military Bitch'},
-    {name: 'Raiden', role: 'Electricity Guy'},
-    {name: 'Liu Kang', role: 'Brus Lee Guy'},
-    {name: 'Goro', role: 'Multitasking Guy'},
-    {name: 'Erron Black', role: 'Cowboy Guy'},
-    {name: 'Predator', role: 'Animal'}
+const userList = [
+    {
+        uid: "SUBZERO",
+        name: "Sub-Zero",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'sub-zero@mortalkombat.com',
+        role: 'Cold Ninja',
+        statusMessage: "I tire of maintaining our innocence.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "SCORPION",
+        name: "Scorpion",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'scorpion@mortalkombat.com',
+        role: 'Burning Ninja',
+        statusMessage: "I will relieve you of your burden!",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "JOHNNYCAGE",
+        name: "Johnny Cage",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'johnnycage@mortalkombat.com',
+        role: 'Law Enforcer',
+        statusMessage: "An actor and a Shaolin walk into a bar.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "SONYABLADE",
+        name: "Sonya Blade",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'sonyablade@mortalkombat.com',
+        role: 'Military Bitch',
+        statusMessage: "You're such an ass.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "RAIDEN",
+        name: "Raiden",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'raiden@mortalkombat.com',
+        role: 'Electrician',
+        statusMessage: "Thunder blots out the sun.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "LIUKANG",
+        name: "Liu Kang",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'liukang@mortalkombat.com',
+        role: 'Brus Lee Guy',
+        statusMessage: "I have no patience for your humor.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "GORO",
+        name: "Goro",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'goro@mortalkombat.com',
+        role: 'Multitasking Guy',
+        statusMessage: "Here is what you are owed.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "ERRONBLACK",
+        name: "Erron Black",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'erronbalck@mortalkombat.com',
+        role: 'Cawboy',
+        statusMessage: "He offered more coins.",
+        lastActiveAt: 1547011919
+    },
+    {
+        uid: "KITANA",
+        name: "Kitana",
+        authToken: "123",
+        avatar: "http://url-to-avatar",
+        link: "http://url-to-profile",
+        status: "online | offline",
+        credits: 1000,
+        email: 'kitana@mortalkombat.com',
+        role: 'Nasty Bitch',
+        statusMessage: "I know of many.",
+        lastActiveAt: 1547011919
+    }
 ];
 
-function getRandomElement() {
-    return mockedData[Math.floor(Math.random() * mockedData.length)];
-}
+const getRandomElement = () => userList[Math.floor(Math.random() * userList.length)];
 
 export const initChat = () => {
     return new Promise((resolve, reject) => {
@@ -48,4 +154,23 @@ export const sendChatMessage = message => {
         }, 100);
     });
 
+};
+
+export const getUserList = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(userList);
+        });
+    });
+    /*const limit = 30;
+    const userRequest = new CometChat.UsersRequestBuilder().setLimit(limit).build();
+
+    userRequest.fetchNext().then(
+        userList => {
+            console.log('user list received: ', userList);
+        },
+        error => {
+            console.log("User list fetching failed with error: ", error);
+        }
+    );*/
 };
