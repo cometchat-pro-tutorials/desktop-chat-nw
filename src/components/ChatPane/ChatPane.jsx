c0631090714fad816c47680b251d64894859d4b7
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-08 11:38:50 +0200

    Updated ChatPane component.
    - Fetch users list and pass it to Participants component.

--- src/components/ChatPane/ChatPane.jsx
+++ src/components/ChatPane/ChatPane.jsx
@@ -1,27 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 import Participants from "../Participants/Participants";
 import Conversation from "../Conversation/Conversation";
-import { init, login } from '../../actions';
+import { init, login, getUsersList } from '../../actions';
 import "./ChatPane.css";
 
-const ChatPane = ({init, login}) => {
+const ChatPane = ({ init, login, getUsersList }) => {
+    const [usersList, setUsersList] = useState([]);
 
     useEffect(() => {
-        init().then(login().then((userData) => console.log('user: ', userData)));
-    });
+        init().then(login().then((userData) => {
+            console.log('logging in user: ', userData);
+            getUsersList().then(usersList => setUsersList(usersList));
+        }));
+    }, []);
 
     return (
         <div className="chat-pane">
-            <Participants />
+            <Participants list={usersList}/>
             <Conversation />
         </div>);
 };
 
 const mapDispatchToProps = (dispatch) => ({
     init: () => dispatch(init()),
-    login: () => dispatch(login())
+    login: () => dispatch(login()),
+    getUsersList: () => dispatch(getUsersList())
 });
 
 export default connect(null, mapDispatchToProps)(ChatPane);
