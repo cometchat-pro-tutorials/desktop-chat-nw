c5c80eb307fa81812ecb81273671ee7ac6f10c5a
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-13 13:05:30 +0200

    Fixed scrolling to bottom when load the page or send a message.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -18,7 +18,7 @@ export const prepareMessages = (messagesData) => {
 export const scrollToBottom = (elementToBeScrolled) => {
   /* Added in setInterval in order to handle the case when the chat is loaded
   or the page is refreshed */
-  setInterval(() => {
+  setTimeout(() => {
     elementToBeScrolled.scrollTop = elementToBeScrolled.scrollHeight;
   });
 
