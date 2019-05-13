568c75450b885fc51de96245d1b1a8c74606d5c6
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-13 17:25:19 +0200

    Added error message for wrong username cases.

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,34 +6,47 @@ import "./Welcome.css";
 
 const Welcome = ({ callback }) => {
     const [username, setUsername] = useState('');
+    const [hasError, setHasError] = useState(false);
 
     const handleClick = () => {
-        storeToLocalStorage('username', username);
-        callback();
+        if ((username === 'superhero1' || username === 'superhero2' || username === 'superhero3' || username === 'superhero4' || username === 'superhero5')) {
+            setHasError(false);
+
+            storeToLocalStorage('username', username);
+            callback();
+        } else {
+            setHasError(true);
+        }
     };
 
     return (
-        <div className="welcome-screen">
-            <form onSubmit={handleClick}>
-                <div className="form-group">
-                    <div><label htmlFor="username">Enter your name</label></div>
-                    <TextField
-                        name="username"
-                        id="outlined-name"
-                        label="Name"
-                        className=""
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        margin="normal"
-                        variant="outlined"
-                        required
-                    />
-                </div>
-                <div>
-                    <Button variant="contained" color="primary" onClick={handleClick}>Enter Chat</Button>
-                </div>
-            </form>
-        </div>
+        <>
+            <div className="welcome-screen">
+                <form onSubmit={handleClick}>
+                    <div className="form-group">
+                        <div>
+                            {hasError &&
+                            <div className="error">User not authenticated. Please use one of the already registered usernames, e.g.
+                                "superhero1"</div>}
+                            <label htmlFor="username">Enter your name</label>
+                        </div>
+                        <TextField
+                            name="username"
+                            id="outlined-name"
+                            label="Name"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            margin="normal"
+                            variant="outlined"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Button variant="contained" color="primary" onClick={handleClick}>Enter Chat</Button>
+                    </div>
+                </form>
+            </div>
+        </>
     );
 };
 
