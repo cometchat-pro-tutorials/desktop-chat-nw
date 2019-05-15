37c052977b5f463b24117b4b69038c3460de578c
Mihail Gaberov <mihail.gaberov@gmail.com>
2019-05-15 15:02:51 +0200

    Added received message into redux store.

--- src/reducers/messageReducer.js
+++ src/reducers/messageReducer.js
@@ -1,25 +1,32 @@
 import {
-  SEND_MESSAGE_SUCCESS,
-  SEND_MESSAGE_BEGIN,
-  SEND_MESSAGE_FAILED
+    SEND_MESSAGE_SUCCESS,
+    SEND_MESSAGE_BEGIN,
+    SEND_MESSAGE_FAILED, MESSAGE_RECEIVED, FETCH_GROUP_CONVERSATIONS_SUCCESS
 } from '../actions';
 import initialState from './initialState';
 
 export default function messageReducer(state = initialState.messages, action) {
-  switch(action.type) {
-    case SEND_MESSAGE_BEGIN:
-      return state;
+    switch (action.type) {
+        case SEND_MESSAGE_BEGIN:
+            return state;
 
-      case SEND_MESSAGE_SUCCESS:
-      return [...state, action.payload];
+        case SEND_MESSAGE_SUCCESS:
+            return [...state, action.payload];
 
-    case SEND_MESSAGE_FAILED:
-      return {
-        ...state,
-        error: action.payload.error
-      };
+        case SEND_MESSAGE_FAILED:
+            return {
+                ...state,
+                error: action.payload.error
+            };
 
-    default:
-      return state;
-  }
+        case FETCH_GROUP_CONVERSATIONS_SUCCESS:
+            return [...state, ...action.payload.conversations];
+
+        case
+        MESSAGE_RECEIVED:
+            return [...state, action.payload];
+
+        default:
+            return state;
+    }
 }
