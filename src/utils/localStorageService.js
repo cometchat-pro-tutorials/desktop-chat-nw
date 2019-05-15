ad37943f73ab57ef82daa1f913425cd1d91370c1
mihailgaberov <mihail.gaberov@gmail.com>
2019-05-15 08:56:06 +0200

    Cleaned localStorageService from redundant func.

--- src/utils/localStorageService.js
+++ src/utils/localStorageService.js
@@ -1,5 +1,3 @@
-import { debounce } from 'lodash';
-
 // Create a record
 export const createRecord = (key, value) => {
     if (!key || !value) { return { error: 'Store to localStorage failed. Invalid key or value.' }; }
@@ -17,31 +15,8 @@ export const updateRecord = (key, value) => {
     localStorage.setItem(key, value);
 };
 
-// Delete a record
-export const deleteRecord = key => {
-    if (!key) { return { error: 'Store to localStorage failed. Invalid key.' }; }
-    localStorage.removeItem(key)
-};
-
-// Clear all
-export const clearAll = () => localStorage.clear();
-
-// Check for stored items
-export const hasStoredItems = () => localStorage.length > 0;
-
 // Check for support
 export const isLocalStorageSupported = () => !!window.localStorage;
-
-// Store (create or update) record to local storage with some delay (e.g. 1500 ms)
-export const storeToLocalStorageDebounced = debounce((key, value) => {
-    if (isLocalStorageSupported()) {
-        if (readRecord(key)) {
-            updateRecord(key, value);
-        } else {
-            createRecord(key, value);
-        }
-    }
-}, 1500, {leading: false});
 
 // Store (create or update) record to local storage
 export const storeToLocalStorage = (key, value) => {
